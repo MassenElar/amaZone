c8988376d3a7e056378147f1d7165261f55ddae8
OussamaElar <oussama.elar@gmail.com>
2021-09-09 23:55:14 -0400

    2nd push to heroku

--- frontend/components/welcome/footer.jsx
+++ frontend/components/welcome/footer.jsx
@@ -3,53 +3,73 @@ import { Link } from 'react-router-dom';
 // import logo from '../../../amaZoneLogo1.png'
 
 
-const Footer = () => {
-      return (
-            <footer className="footer">
-                  <div className="footer-signin">
-                        <p>See personalized recommendations</p>
-                        <button className="submit-button"><Link className="botton-footer-link" to="/login">Sign In</Link></button>
-                        <p className="new-cost-footer">New customer? <span><Link className="footer-span" to="/signup">Start Here</Link></span></p>
-                  </div>
-                  <div className="footer-items">
+class Footer extends React.Component {
+      constructor(props) {
+            super(props)
+      }
+
+
+      render() {
+            const {currentUser} = this.props
+            let footerSignin;
+            if (currentUser !== undefined) {
+                  footerSignin = null;
+            } else {
+                  footerSignin = <div className="footer-signin">
+                                    <p>See personalized recommendations</p>
+                                    <button className="submit-button"><Link className="botton-footer-link" to="/login">Sign In</Link></button>
+                                    <p className="new-cost-footer">New customer? <span><Link className="footer-span" to="/signup">Start Here</Link></span></p>
+                              </div>
+            }
+            return (
+                  <footer className="footer">
                         
-                        <div className="back-to-top">
-                              <p>back to Top</p>
+                        {/* <div className="footer-signin">
+                              <p>See personalized recommendations</p>
+                              <button className="submit-button"><Link className="botton-footer-link" to="/login">Sign In</Link></button>
+                              <p className="new-cost-footer">New customer? <span><Link className="footer-span" to="/signup">Start Here</Link></span></p>
+                        </div> */}
+                        {footerSignin}
+                        <div className="footer-items">
+                              <div className="back-to-top">
+                                    <p>back to Top</p>
+                              </div>
+                              <div className="footer-list">
+                                    <ul>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                    </ul>
+                                    <ul>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                    </ul>
+                                    <ul>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                          <li>Get to Know Us</li>
+                                    </ul>
+                              </div>
+                              <div className="footer-links">
+                                    <img className="logo-footer" src={window.amaZoneLogo1URL}></img>
+                                    <i className="fa fa-github" ></i>
+                                    <i className="fa fa-linkedin-square"></i>
+                              </div>
                         </div>
-                        <div className="footer-list">
-                              <ul>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                              </ul>
-                              <ul>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                              </ul>
-                              <ul>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                                    <li>Get to Know Us</li>
-                              </ul>
-                        </div>
-                        <div className="footer-links">
-                              <img className="logo-footer" src={window.amaZoneLogo1URL}></img>
-                              <i className="fa fa-github" ></i>
-                              <i className="fa fa-linkedin-square"></i>
-                        </div>
-                  </div>
-            </footer>
-      )
+                  </footer>
+            )
+      }
+
 
 }
 
--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -3,75 +3,98 @@ import { Link } from 'react-router-dom';
 // import logo from '../../../amaZoneLogo1.png'
 
 
-const Welcome = ({ currentUser, logout }) => {
-      let user;
-      let logOutButton;
-      if (currentUser) {
-            
-                  
-            user = currentUser.username
-            logOutButton = <button onClick={logout}>Logout</button>
-                  
-            
-      } else {
-            user = "Sign In"
-            logOutButton = null
+class Welcome extends React.Component {
+      constructor(props) {
+            super(props)
+            this.state = {
+                  show: false,
+            }
+            this.whenHover = this.whenHover.bind(this);
       }
-      // else {
-      //       return (
-      //             <div>
-      //                   <Link to="/signup">SignUp</Link>
-      //                   <br></br>
-      //                   <Link to="/login">Login</Link>
-      //             </div>
-      //       )
-      // }
 
-      return (
-            <div>
-                  <nav className="navbar">
-                        <Link to="/">
-                              <img src={window.amaZoneLogo1URL} className="logo"></img>
-                        </Link>
-                        <Link to="/login" className="signin-link">
-                                    <div className="signin-menu">
-                                    <span className="menu1">{ currentUser ? "Deliver to " + user : "Hello" }</span>
-                                          <span className="menu2"><i className="fa fa-map-marker"></i>Select your address</span>
-                                    </div>
-                        </Link>
-                        <div className="search">
-                              <input type="text" className="search-input"></input>
-                              <i className="fa fa-search" aria-hidden="true"></i>
-                        </div>
-                        <div className="navright">
+      whenHover(e) {
+            const newState = !this.state.show 
+            this.setState({show: newState})
+    }
+      
+      render(){
+            const { currentUser, logout } = this.props
+            let user;
+            let logOutButton;
+            let signInButton;
+            let signUpLink;
+            if (currentUser) {
+                  
+                        
+                  user = currentUser.username
+                  logOutButton = <button className="drp-signin-btn" onClick={logout}>Logout</button>
+                  signInButton = null
+                  signUpLink = null 
+                  
+            } else {
+                  user = "Sign In"
+                  logOutButton = null
+                  signInButton = <Link to="/login"><button className="drp-signin-btn">Sign In</button></Link>
+                  signUpLink = <p>New customer? <span ><Link className="drp-signup-span" to="signup"> Start Here</Link></span></p>
+
+            }
+            // else {
+            //       return (
+            //             <div>
+            //                   <Link to="/signup">SignUp</Link>
+            //                   <br></br>
+            //                   <Link to="/login">Login</Link>
+            //             </div>
+            //       )
+            // }
+
+            return (
+                  <div>
+                        <nav className="navbar">
+                              <Link to="/">
+                                    <img src={window.amaZoneLogo1URL} className="logo"></img>
+                              </Link>
                               <Link to="/login" className="signin-link">
-                                    <div className="signin-menu">
-                                          <span className="menu1">Hello, {user}</span>
-                                          <span className="menu2">Account & Lists</span>
-                                    </div>
+                                          <div className="signin-menu">
+                                          <span className="menu1">{ currentUser ? "Deliver to " + user : "Hello" }</span>
+                                                <span className="menu2"><i className="fa fa-map-marker"></i>Select your address</span>
+                                          </div>
                               </Link>
-                              <div  className="signin-link">
-                                    <div className="signin-menu">
-                                          <span className="menu1">Returns</span>
-                                          <span className="menu2">& Orders</span>
-                                    </div>
-                                    {/* <div className="dorp-down-list">
-                                          <button>Sign In</button>
-                                          <p>New customer? <span><Link to="signup">Start Here</Link></span></p>
-                                    </div> */}
+                              <div className="search">
+                                    <input type="text" className="search-input"></input>
+                                    <i className="fa fa-search" aria-hidden="true"></i>
                               </div>
-                              <Link to="/" className="signin-link">
-                                    <div className="shopping-cart">
-                                          <i className="fa fa-shopping-cart"></i>
+                              <div className="navright">
+                                    <div className="signin-link">
+                                          <div className="signin-menu" onMouseOver={this.whenHover} >
+                                                <span className="menu1">Hello, {user}</span>
+                                                <span className="menu2">Account & Lists</span>
+                                                <div onMouseOver={e => e.stopPropagation()} onMouseLeave={this.whenHover} className={this.state.show ? "show" : "clear"}>
+                                                      {logOutButton}
+                                                      {signInButton}
+                                                      {signUpLink} 
+                                                </div>
+                                          </div>
                                     </div>
-                              </Link>
-                  
-                        </div>
-                  </nav>
-                  
-                  {logOutButton}
-            </div>
-      )
+                                    <div  className="signin-link">
+                                          <div className="signin-menu" >
+                                                <span className="menu1">Returns</span>
+                                                <span className="menu2">& Orders</span>
+                                                
+                                          </div>
+                                    </div>
+                                    <Link to="/" className="signin-link">
+                                          <div className="shopping-cart">
+                                                <i className="fa fa-shopping-cart"></i>
+                                          </div>
+                                    </Link>
+                        
+                              </div>
+                        </nav>
+                        
+                  </div>
+            )
+      }
 
 }
 
